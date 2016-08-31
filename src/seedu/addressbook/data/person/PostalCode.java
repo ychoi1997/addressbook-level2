b2bef235e2dec4f6d33deb87be17ddbe32de2ab4
Ying Ying Choi <yingying_choi@hotmail.com>
2016-08-31 23:01:29 +0800

    Added PostalCode class

---
+++ src/seedu/addressbook/data/person/PostalCode.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * @author Ying Ying Choi
+ *
+ */
+public class PostalCode {
+	private final String value;
+	public PostalCode(String value) {
+		this.value = value;
+	}
+	public String getPostalCode() {
+		return value;
+	}
+}
