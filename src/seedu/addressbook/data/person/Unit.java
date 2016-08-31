d4afdfc3be8f2b8961b9a8506cd1543d6c3442b3
Ying Ying Choi <yingying_choi@hotmail.com>
2016-08-31 23:01:58 +0800

    Added Unit class

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * @author Ying Ying Choi
+ *
+ */
+public class Unit {
+	private final String value;
+	public Unit(String value) {
+		this.value = value;
+	}
+	public String getUnit() {
+		return value;
+	}
+}
