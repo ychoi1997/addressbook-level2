03459900aa1e527b43725bf5c409148ed8bc0046
Ying Ying Choi <yingying_choi@hotmail.com>
2016-08-31 23:01:02 +0800

    Added Block class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * @author Ying Ying Choi
+ *
+ */
+public class Block {
+	private final String value;
+	public Block(String value){
+		this.value = value;
+	}
+	/**
+	 * @return the value
+	 */
+	public String getBlock() {
+		return value;
+	}
+}
