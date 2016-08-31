59a923b2849dd1a87cfcf99b5ced36bd6bf64116
Ying Ying Choi <yingying_choi@hotmail.com>
2016-08-31 23:01:44 +0800

    Added Street class

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package seedu.addressbook.data.person;
+
+/**
+ * @author Ying Ying Choi
+ *
+ */
+public class Street {
+	private final String value;
+	public Street(String value) {
+		this.value = value;
+	}
+	public String getStreet() {
+		return value;
+	}
+}
